efe39a09a9675912cf003993a709d5e9a6af5fc5
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2026-03-28 16:44:06 +0530

    change the string

--- basic/shows-no-of-character-typed.js
+++ basic/shows-no-of-character-typed.js
@@ -1,4 +1,4 @@
 /* Shows the no. of characters in the string */
-str1 = "This is a string in which want to show the number of characters.";
+str1 = "This is a sample string.";
 numofchar = length.str1;
 console.log(numofchar);
